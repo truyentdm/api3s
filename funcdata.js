8e84d96f9f7b9d6d56be3b7655eb287f0764f995
truyentdm <truyentdm@gmail.com>
2020-06-26 00:46:48 +0700

    Update funcdata.js

--- funcdata.js
+++ funcdata.js
@@ -1,11 +1,27 @@
 //
 function checkLocal(objURL){
-		for (var i = 0; i < objURL.length; i++) {
-			 if (objURL[i] == "US" || objURL[i] == "CA" || objURL[i] == "UK") {
-				console.log("checkLocal>>>>>>>>>",true)
-				return true;
-			}
+	for (var i = 0; i < objURL.length; i++) {
+		 if (objURL[i] == "US" || objURL[i] == "CA" || objURL[i] == "UK") {
+			console.log("checkLocal>>>>>>>>>",true)
+			return true;
 		}
-		console.log("checkLocal>>>>>>>>>",false)
-		return false;
 	}
+	console.log("checkLocal>>>>>>>>>",false)
+	return false;
+}
+
+function getLocal(objURL){
+	if(checkLocal(objURL)){
+		if (objURL[1] == "US") { 
+			return "US";
+		}else if(objURL[1] == "CA"){
+			return "CA";
+		}else if(objURL[1] == "UK"){
+			return "UK";
+		}else{
+			return false;
+		}
+		
+	}
+	return false;
+}
